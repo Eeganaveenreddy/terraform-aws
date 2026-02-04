b1731f481b095beb236b537c2f6819fcd2b712b1
naveen <enreddy619@gmail.com>
2026-02-04 11:43:20 +0000

    gfdgdf

--- modules/db/aws_ebs_volume.tf
+++ modules/db/aws_ebs_volume.tf
@@ -1,23 +1,50 @@
-resource "aws_ebs_volume" "db_data" {
-  # count = var.server_name == "db-terraform" ? 1 : 0
+# resource "aws_ebs_volume" "db_data" {
+#   count = length(data.aws_ebs_volumes.existing_db.ids) > 0 ? 0 : 1
 
-  availability_zone = data.aws_subnet.private.availability_zone
-  size              = 250
-  type              = "gp3"
+#   availability_zone = data.aws_subnet.private.availability_zone
+#   size              = 250
+#   type              = "gp3"
 
-  tags = {
-    Name        = "${var.env}-db-data"
-    Environment = var.env
-    Role        = "database"
-  }
-}
+#   tags = {
+#     Name        = "${var.env}-db-data"
+#     Environment = var.env
+#     Role        = "database"
+#   }
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
 
-resource "aws_volume_attachment" "db_attach" {
-  count = var.server_name == "db-terraform" ? 1 : 0
+# resource "aws_volume_attachment" "db_attach" {
+#   count = var.server_name == "db-terraform" ? 1 : 0
 
-  device_name = "/dev/sdf"
-  volume_id   = aws_ebs_volume.db_data.id
-  instance_id = aws_instance.db_instance.id
+#   device_name = "/dev/sdf"
+#   volume_id   = local.db_volume_id
+#   instance_id = aws_instance.db_instance.id
 
-  force_detach = true
-}
+#   force_detach = true
+#   depends_on = [
+#     data.aws_ebs_volumes.existing_db,
+#     aws_ebs_volume.db_data
+#   ]
+# }
+
+# data "aws_ebs_volumes" "existing_db" {
+#   filter {
+#     name   = "tag:Name"
+#     values = ["${var.env}-db-data"]
+#   }
+#   filter {
+#     name   = "availability-zone"
+#     values = [data.aws_subnet.private.availability_zone]
+#   }
+# }
+
+# locals {
+#   db_volume_id = try(
+#     data.aws_ebs_volumes.existing_db.ids[0],
+#     aws_ebs_volume.db_data[0].id
+#   )
+# }
+
+
--- modules/db/mount_disk_on_dbserver.sh
+++ modules/db/mount_disk_on_dbserver.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
-set -eux
+# #!/bin/bash
+# set -eux
 
-MOUNT="/data"
+# MOUNT="/data"
 
-# Find the 250G disk dynamically
-DEVICE=$(lsblk -dn -o NAME,SIZE | awk '$2 ~ /250G/ {print "/dev/"$1}')
+# # Find the 250G disk dynamically
+# DEVICE=$(lsblk -dn -o NAME,SIZE | awk '$2 ~ /250G/ {print "/dev/"$1}')
 
-while [ -z "$DEVICE" ]; do
-  sleep 3
-  DEVICE=$(lsblk -dn -o NAME,SIZE | awk '$2 ~ /250G/ {print "/dev/"$1}')
-done
+# while [ -z "$DEVICE" ]; do
+#   sleep 3
+#   DEVICE=$(lsblk -dn -o NAME,SIZE | awk '$2 ~ /250G/ {print "/dev/"$1}')
+# done
 
-blkid $DEVICE || mkfs.ext4 $DEVICE
+# blkid $DEVICE || mkfs.ext4 $DEVICE
 
-mkdir -p $MOUNT
-mount $DEVICE $MOUNT
-echo "$DEVICE $MOUNT ext4 defaults,nofail 0 2" >> /etc/fstab
+# mkdir -p $MOUNT
+# mount $DEVICE $MOUNT
+# echo "$DEVICE $MOUNT ext4 defaults,nofail 0 2" >> /etc/fstab
