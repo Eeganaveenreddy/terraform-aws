0890305808811a4046c38e15a79f1ea55fbdf580
naveen <enreddy619@gmail.com>
2026-02-11 17:41:04 +0000

    gfdgd

---
+++ modules/iam/ssm-iam-policy.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_read" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "SSMManagedInstanceCore" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
