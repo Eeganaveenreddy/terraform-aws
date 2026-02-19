cdd33cc9fc4c2ddc64165f5b2f7349c36c6cae7a
naveen <enreddy619@gmail.com>
2026-02-19 07:35:19 +0000

    changed idle time 60 to 300 in load balancer.

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -4,6 +4,7 @@ resource "aws_lb" "test" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
   subnets            = var.public_subnet_id
+  idle_timeout       = 300
 
   # enable_deletion_protection = true
 
